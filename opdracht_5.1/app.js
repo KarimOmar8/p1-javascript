32d75c251b00a143f74a25c0f39b7532fac27912
KarimOmar8 <9017210@student.zadkine.nl>
2022-10-03 19:05:06 +0200

    opd5.1

---
+++ opdracht_5.1/app.js
@@ -0,0 +1,4 @@
+document.getElementById("h1").style.color = "purple";
+document.getElementById("h1").innerText = "ik ben karim";
+document.getElementById("p").innerText = "p text"
+document.getElementById("p").style.fontSize = "5rem"
