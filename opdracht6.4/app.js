72e94364fd4263ac539ce5ef83d93e90d7c45d99
KarimOmar8 <113107665+KarimOmar8@users.noreply.github.com>
2022-10-07 09:46:58 +0200

    opd6.4

---
+++ opdracht6.4/app.js
@@ -0,0 +1,22 @@
+function calculater(x, y) {
+let answer = x + y;
+console.log(answer);
+}
+calculater (6, 5);
+
+
+
+function multiplybyfive(x) {
+    let answer = x * 5;
+    console.log(answer);
+    }
+    multiplybyfive (x);
+
+
+    function calculatermintutes(num){
+        let answer = num /60;
+        console.log(num / 60)
+
+    }
+
+    calculatermintutes(3600)
