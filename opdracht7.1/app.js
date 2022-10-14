906ab84a3c0b33b0c3cdaefcad5cfba721308226
KarimOmar8 <113107665+KarimOmar8@users.noreply.github.com>
2022-10-14 10:06:11 +0200

    opd 7.1

---
+++ opdracht7.1/app.js
@@ -0,0 +1,14 @@
+let budget = 100;
+let product = 60;
+
+
+if( budget > 60){
+    console.log ("je hebt genoeg geld!");
+}
+else{
+    console.log (" helaas, je hebt geen genoeg geld");
+}
+
+function kleur_aanpassen(){
+    document.getElementById("budget").style.color ="red";
+}
